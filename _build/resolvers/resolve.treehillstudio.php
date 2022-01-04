0bb082e6e5dc01d1612bbaff0a640335107d661a
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-04 11:58:20 +0100

    Normalize code

--- _build/resolvers/resolve.treehillstudio.php
+++ _build/resolvers/resolve.treehillstudio.php
@@ -2,11 +2,11 @@
 /**
  * Resolves install statistics
  *
- * @package FilterWhere
+ * @package filterwhere
  * @subpackage build
  *
- * @var mixed $object
  * @var array $options
+ * @var xPDOObject $object
  * @var xPDOTransport $transport
  */
 
@@ -34,10 +34,8 @@ $c->where(
 );
 $c->where(
     [
-        [
-            'modTransportPackage.signature:LIKE' => $options['namespace'] . '-%',
-        ],
-        'installed:IS NOT' => null
+        'modTransportPackage.signature:LIKE' => $options['namespace'] . '-%',
+        'modTransportPackage.installed:IS NOT' => null
     ]
 );
 $c->limit(1);
