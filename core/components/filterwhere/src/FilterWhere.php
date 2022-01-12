7e4b7eb60f6fb63fd0131e0a16f2a3c4b231a9a4
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-12 15:19:00 +0100

    Normalize code

--- core/components/filterwhere/src/FilterWhere.php
+++ core/components/filterwhere/src/FilterWhere.php
@@ -55,7 +55,7 @@ class FilterWhere
      */
     public function __construct(modX &$modx, array $options = [])
     {
-        $this->modx = &$modx;
+        $this->modx =& $modx;
         $this->namespace = $this->getOption('namespace', $options, $this->namespace);
 
         $corePath = $this->getOption('core_path', $options, $this->modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/' . $this->namespace . '/');
