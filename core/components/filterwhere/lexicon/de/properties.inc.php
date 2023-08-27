b4eb2c34a91af8e5bbd5301c72ab0fe7a96f5212
Thomas Jakobi <buero@treehillstudio.de>
2023-08-27 17:26:15 +0200

    Added translation using Weblate (German)

---
+++ core/components/filterwhere/lexicon/de/properties.inc.php
@@ -0,0 +1,6 @@
+<?php
+$_lang['filterwhere.filtergetresourceswhere.emptyRedirect'] = '';
+$_lang['filterwhere.filtergetresourceswhere.fields'] = '';
+$_lang['filterwhere.filtergetresourceswhere.toPlaceholder'] = '';
+$_lang['filterwhere.filtergetresourceswhere.where'] = '';
+$_lang['filterwhere.filtergetresourceswhere.varName'] = '';
