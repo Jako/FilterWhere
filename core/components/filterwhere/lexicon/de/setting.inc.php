0e078da0deec2805a341da8cb8a6e78b937a64cd
Thomas Jakobi <buero@treehillstudio.de>
2023-08-27 15:28:31 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: MODX Extras/MODX FilterWhere/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-filterwhere-system-settings/de/

--- core/components/filterwhere/lexicon/de/setting.inc.php
+++ core/components/filterwhere/lexicon/de/setting.inc.php
@@ -5,10 +5,10 @@
  * @package filterwhere
  * @subpackage lexicon
  */
-$_lang['area_geocode'] = '';
-$_lang['setting_filterwhere.debug'] = '';
-$_lang['setting_filterwhere.debug_desc'] = '';
-$_lang['setting_filterwhere.google_maps_api_key'] = '';
-$_lang['setting_filterwhere.google_maps_api_key_desc'] = '';
-$_lang['setting_filterwhere.google_maps_region'] = '';
-$_lang['setting_filterwhere.google_maps_region_desc'] = '';
+$_lang['area_geocode'] = 'Geocode';
+$_lang['setting_filterwhere.debug'] = 'Debug';
+$_lang['setting_filterwhere.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
+$_lang['setting_filterwhere.google_maps_api_key'] = 'Google Maps Geokodierungs API-Schlüssel';
+$_lang['setting_filterwhere.google_maps_api_key_desc'] = '<a href="https://developers.google.com/maps/documentation/javascript/get-api-key">Erzeuge</a> einen Google Maps API-Schlüssel mit aktivierter Geocoding-API.';
+$_lang['setting_filterwhere.google_maps_region'] = 'Bevorzugte Google Maps API Region';
+$_lang['setting_filterwhere.google_maps_region_desc'] = '<a href="https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingRegionCodes">Bevorzugte Region</a> für das Geokodierungsergebnis.';
