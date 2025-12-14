Jako/PDFResource

Added translation using Weblate (Spanish)

core/components/pdfresource/lexicon/es/default.inc.php
@@ -1,0 +1,8 @@
+<?php
+/**
+ * Default Lexicon Entries for PDFResource
+ *
+ * @package pdfresource
+ * @subpackage lexicon
+ */
+$_lang['pdfresource'] = '';

Jako/FilterWhere

core/components/filterwhere/lexicon/es/default.inc.php
@@ -1,0 +1,8 @@
+<?php
+/**
+ * Default Lexicon Entries for FilterWhere
+ *
+ * @package filterwhere
+ * @subpackage lexicon
+ */
+$_lang['filterwhere'] = '';